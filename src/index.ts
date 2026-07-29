4c7e1a8b8af53a1b02a5d14c427adb87239a2ba2
Pobphut <phopphut@gmail.com>
2026-07-29 18:11:39 +0700

    เทส

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,6 @@ import studentRouter_v3 from "./routes/studentsRoutes_v3.js";
 import courseRouter_v2 from "./routes/coursesRouters_v2.js";
 import userRouter_v2 from "./routes/userRouters.js";
 import enrollment from "./routes/enrollmentRouters.js";
-import { success } from "zod";
-
-
 
 const app = express();
 const port = 3000;
@@ -23,22 +20,14 @@ app.use(express.json());
 
 // logger middleware
 app.use(morgan("dev"));
-// app.use(morgan("combined"));
 
 // JSON parser middleware
 app.use(invalidJsonMiddleware);
 
-// Endpoints
+// Base Endpoints
 app.get("/", (req: Request, res: Response) => {
   res.send("Lecture09 API services");
 });
-
-
-app.use("/api/v2/students", studentRouter_v2);
-app.use("/api/v3/students", studentRouter_v3);
-app.use("/api/v2/courses", courseRouter_v2);
-app.use("/api/v2/users", userRouter_v2);
-app.use("/api/v2/enrollments", enrollment);
 
 app.get("/api/me", (req: Request, res: Response) => {
   return res.status(200).json({
@@ -49,12 +38,19 @@ app.get("/api/me", (req: Request, res: Response) => {
       firstName: "Pobphut",
       lastName: "kungwong",
       program: "CPE",
-      section:"001",
+      section: "001",
     },
   });
-})
+});
 
-// endpoint check middleware
+// API Routes
+app.use("/api/v2/students", studentRouter_v2);
+app.use("/api/v3/students", studentRouter_v3);
+app.use("/api/v2/courses", courseRouter_v2);
+app.use("/api/v2/users", userRouter_v2);
+app.use("/api/v2/enrollments", enrollment);
+
+// 404 Not Found middleware (วางไว้ท้ายสุดเสมอ)
 app.use(notFoundMiddleware);
 
 app.listen(port, () => {
